29bfc5bdf560d7e633fb0be0aa27ae238302c563
Sylvictu <92850412+Sylvictu@users.noreply.github.com>
2021-12-13 18:45:13 +0100

    Add files via upload

---
+++ Opdracht 10/script.js
@@ -0,0 +1,23 @@
+console.log("Hello WincAcedemy");
+
+let name = "Peter van beek";
+console.log(name);
+//let if = test("This is a test");
+let firststring = "4";
+let secondstring = "4";
+let firstint = 4;
+let secondint = 4;
+console.log(4 + 4);
+console.log(firstint + secondint);
+console.log(firststring + secondstring);
+console.log(10 + 8);
+console.log(10 - 8);
+console.log(10 * 8);
+console.log(10 / 8);
+console.log(10 % 8);
+let myage = 31;
+console.log(typeof myage);
+let myage2 = "31";
+console.log(typeof myage2);
+
+
