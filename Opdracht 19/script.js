8896a29938697336063cc376b05eb3128b50bdb0
Sylvictu <92850412+Sylvictu@users.noreply.github.com>
2021-12-18 20:08:46 +0100

    Add files via upload

---
+++ Opdracht 19/script.js
@@ -0,0 +1,35 @@
+// Part 1 Hey Kiddo
+const ageCheck = function (age) {
+    if (age >= 18) {
+        return true;
+    }
+    else {
+        return false;
+    };
+};
+
+const agend = function (age) {
+    if (ageCheck(age) == true) {
+        return "Hello there";
+    }
+    else {
+        return "Hey kiddo";
+    }
+}
+console.log(agend(18));
+
+// Part 2 VAT excercise 1
+
+const vatNone = function (basePrice, vatPercentage) {
+    return basePrice * vatPercentage
+}
+
+const totalPrice = function (basePrice, vatPercentage) {
+    return (vatNone(basePrice, vatPercentage))
+}
+
+console.log("The total price including VAT is:", Math.round(totalPrice(1000, 1.20)));
+
+
+// Part 3 VAT excercise 2
+
