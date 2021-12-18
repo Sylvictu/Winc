5793a336b507e75dd4db9da19a36afb18853ae8b
Sylvictu <92850412+Sylvictu@users.noreply.github.com>
2021-12-18 11:03:42 +0100

    Add files via upload

--- Opdracht 16/script.js
+++ Opdracht 16/script.js
@@ -1,8 +1,6 @@
 
 const paintWalls = function (paintColor, wallPicker) {
-    paintColor = "red";
-    wallPicker = "North wall"
     console.log("The", wallPicker, "has been painted", paintColor);
 };
 
-paintWalls();
+paintWalls("red", "north wall");
