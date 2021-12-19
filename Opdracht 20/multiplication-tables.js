03bb0a7a54fdb50c8af97290e87b6e76fd3d3465
Sylvictu <92850412+Sylvictu@users.noreply.github.com>
2021-12-19 11:46:42 +0100

    Add files via upload

---
+++ Opdracht 20/multiplication-tables.js
@@ -0,0 +1,34 @@
+for (i = 1; i < 11; i++) {
+    for (i = 1; i < 11; i++) {
+        console.log(`1 x ${i}  =`, i * 1)
+    }
+    for (i = 1; i < 11; i++) {
+        console.log(`2 x ${i}  =`, i * 2)
+    }
+    for (i = 1; i < 11; i++) {
+        console.log(`3 x ${i}  =`, i * 3)
+    }
+    for (i = 1; i < 11; i++) {
+        console.log(`4 x ${i}  =`, i * 4)
+    }
+    for (i = 1; i < 11; i++) {
+        console.log(`5 x ${i}  =`, i * 5)
+    }
+    for (i = 1; i < 11; i++) {
+        console.log(`6 x ${i}  =`, i * 6)
+    }
+    for (i = 1; i < 11; i++) {
+        console.log(`7 x ${i}  =`, i * 7)
+    }
+    for (i = 1; i < 11; i++) {
+        console.log(`8 x ${i}  =`, i * 8)
+    }
+    for (i = 1; i < 11; i++) {
+        console.log(`9 x ${i}  =`, i * 9)
+    }
+    for (i = 1; i < 11; i++) {
+        console.log(`10 x ${i}  =`, i * 10)
+
+    }
+
+}
